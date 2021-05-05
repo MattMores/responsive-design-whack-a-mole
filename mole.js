c47b13e20f3402e1024012baa93a00b230290b38
Matt Mores <matthewmores@gmail.com>
2021-05-05 15:04:23 -0500

    after lunch

--- mole.js
+++ mole.js
@@ -1,1 +1,10 @@
-// Write your JS here.
+window.addEventListener('DOMContentLoaded', () => {
+
+    setInterval(() => {
+      const moleHeads = document.querySelectorAll('.mole');
+      for (let moleHead of moleHeads) {
+        moleHead.classList.toggle('gameSpace__mole--hidden');
+      }
+    }, 1000);
+
+  });
