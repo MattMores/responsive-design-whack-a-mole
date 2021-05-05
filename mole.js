7875073a5185c8a2e7d1bb2d2b2d4409c07e9dfd
Machaelmus <michaeljtufo@gmail.com>
2021-05-05 16:43:19 -0400

    random number generator

--- mole.js
+++ mole.js
@@ -7,4 +7,15 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     }, 1000);
 
-  });
+  let moles = document.querySelectorAll('.gameSpace__mole--hidden');
+  console.log(moles)
+
+  function popUpRandomMole(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    }
+});
+
+// console.log(popUpRandomMole(0, 7));
+//THICC MOLES
